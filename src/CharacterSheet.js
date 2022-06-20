a36f7c6e16980702bb457fdd614a1a22921e2afa
nashpatty <m.malhan.niazi@gmail.com>
2022-06-19 23:09:37 -0400

    project init and clean up

---
+++ src/CharacterSheet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './character-sheet.css';
+
+class CharacterSheet extends React.Component {
+    return() {
+        <div className="character-sheet">
+            <h1>characteristics</h1>
+        </div>;
+    }
+}
+
+export default CharacterSheet;
