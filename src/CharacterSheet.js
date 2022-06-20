2bd29d1221870e4e08d273548b0b1b4ed592b5d7
nashpatty <m.malhan.niazi@gmail.com>
2022-06-20 01:00:34 -0400

    added half and fifth reactive values to str characteristic, and some styling

--- src/CharacterSheet.js
+++ src/CharacterSheet.js
@@ -1,11 +1,72 @@
 import React from 'react';
-import './character-sheet.css';
 
 class CharacterSheet extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            characteristics: {
+                str: 50,
+                dex: 60,
+                int: 35,
+                con: 65,
+                app: 40,
+                pow: 10,
+                siz: 80,
+                edu: 35,
+                mov: 8,
+            },
+        };
+        // TODO huh? what's going on here?
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        const characteristics = this.state.characteristics;
+        characteristics.str = event.target.value;
+        this.setState({
+            characteristics: characteristics,
+        });
+    }
+
+    hardSuccess(num) {
+        return Math.floor(num * 0.5);
+    }
+
+    extremeSuccess(num) {
+        return Math.floor(num * 0.2);
+    }
+
     render() {
         return (
-            <div className="character-sheet">
-                <h1>characteristics</h1>
+            <div className="font-mono sm:container sm:mx-auto p-4">
+                <div>characteristics</div>
+                <div className="grid md:grid-cols-3 gap-4">
+                    <div className="flex justify-evenly">
+                        <label htmlFor="" className="px-2">
+                            STR
+                        </label>
+                        <input
+                            type="number"
+                            className="rounded border w-12 text-sm"
+                            value={this.state.characteristics.str}
+                            onChange={this.handleChange}
+                        />
+                        <input
+                            type="number"
+                            className="rounded border w-12 text-sm read-only"
+                            value={this.hardSuccess(
+                                this.state.characteristics.str
+                            )}
+                        />
+                        <input
+                            type="number"
+                            className="rounded border w-12 text-sm read-only"
+                            value={this.extremeSuccess(
+                                this.state.characteristics.str
+                            )}
+                        />
+                    </div>
+                </div>
             </div>
         );
     }
