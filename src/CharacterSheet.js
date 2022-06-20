22df4cbab64dd05c6d494a9c29ad733fa319df14
nashpatty <m.malhan.niazi@gmail.com>
2022-06-19 23:20:17 -0400

    changed return to render

--- src/CharacterSheet.js
+++ src/CharacterSheet.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './character-sheet.css';
 
 class CharacterSheet extends React.Component {
-    return() {
-        <div className="character-sheet">
-            <h1>characteristics</h1>
-        </div>;
+    render() {
+        return (
+            <div className="character-sheet">
+                <h1>characteristics</h1>
+            </div>
+        );
     }
 }
 
